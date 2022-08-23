ba8bdf6a78985da36c94bc3851ef6a001b4f6ed4
Lukasz.Gocek <lukasz.gocek@geeks.ltd.uk>
2022-08-23 17:05:17 +0100

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,71 @@
+﻿using ConsoleApp1;
+
+void WelcomeMessage() 
+{
+Console.WriteLine("Hi, I'm Nat Hunt. Welcome to the Resource Manager!");
+Console.WriteLine("Please choose from the following options:");
+Console.WriteLine("Press 'r' for the list of resources available");
+if (InputChecker(new string[] {"r"}))
+    { 
+    Console.WriteLine("");
+    ResourceManagerPage();
+    }
+else
+    {
+        Console.WriteLine("Input not recognised...");
+        WelcomeMessage();
+    };
+}
+
+
+void ResourceManagerPage() 
+{ 
+Console.WriteLine($"You currently have {ResourceManager.ResourcesList.Count} Types of resources");
+
+    if (ResourceManager.ResourcesList.Count > 0)
+        {
+        string x = string.Empty;
+
+        foreach (var item in ResourceManager.ResourcesList)
+            {
+                x = item + ", ";
+            }
+            Console.WriteLine($"Your resources are: {x}");
+        }
+
+Console.WriteLine("Press 'w' to create Wood resource");
+Console.WriteLine("Press 's' to create Stone resource");
+Console.WriteLine("Press 'g' to create Gold resource");
+}
+
+bool InputChecker(string[] wanted_answers)
+{
+    string actual_answer = Console.ReadKey().Key.ToString().ToLower();
+    foreach (var item in wanted_answers)
+    {
+        if (item == actual_answer)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+//Program
+WelcomeMessage();
+
+
+    
+
+    
+    
+
+
+
+
+
+
+
+
---
+++ ConsoleApp1/ResourceManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class ResourceManager
+    {
+        public string ResourceName { get; set; }
+        public float ResourceCounter { get; set; }
+        public float UpgradeRequiredAmount { get; set; }
+        public float UpgradeCounter { get; set; } 
+        public float Modifier { get; set;}   
+        public static List<string> ?ResourcesList = new List<string>{ };
+        public void ShowResources()
+        {
+            Console.WriteLine($"You now have {ResourceCounter} of {ResourceName}!");
+        }
+        public void GetResources(int resources) 
+            { 
+                ResourceCounter+= resources * Modifier;
+            Console.WriteLine($"{resources * Modifier} {ResourceName} obtained! You now have {ResourceCounter} of {ResourceName}!");
+        }
+        public void SpendResources(int amount) 
+            { 
+                ResourceCounter -= amount;
+                UpgradeCounter += amount;
+                Console.WriteLine($"You now have {ResourceCounter} of {ResourceName}!");
+                
+                if (UpgradeCounter >= UpgradeRequiredAmount) 
+                {
+                    Upgrade();
+                }
+        }
+
+        public void Upgrade() 
+            { 
+                Modifier *= 1.1f;
+                UpgradeCounter *= 1.2f;
+                Console.WriteLine($"{ResourceName} upgraded! Next level will cost {UpgradeCounter} of {ResourceName}");
+            }
+
+
+        public ResourceManager(string resourceName)
+        {
+            ResourceName = resourceName;
+            ResourceCounter = 100;
+            UpgradeRequiredAmount = 50;
+            Modifier = 1;
+            ResourcesList.Add(resourceName);
+
+            Console.WriteLine($"New resource of {resourceName} created");
+        }
+    }
+}

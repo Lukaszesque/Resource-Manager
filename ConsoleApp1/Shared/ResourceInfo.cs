589a29d0bc22ea46a9912fca09ca2e86190d4a83
Lukasz.Gocek <lukasz.gocek@geeks.ltd.uk>
2022-11-17 15:55:53 +0000

    Refactors the way that the user adds to resources

--- ConsoleApp1/Shared/ResourceInfo.cs
+++ ConsoleApp1/Shared/ResourceInfo.cs
@@ -13,7 +13,7 @@ namespace ConsoleApp1.Shared
 {
     internal class ItemInfo
     {
-        public void ViewStatus(List<EntityTypes> entityList, string entityType)
+        internal void ViewStatus(List<EntityTypes> entityList, string entityType)
         {
             if (entityList.Count == 0)
             {
@@ -34,46 +34,75 @@ namespace ConsoleApp1.Shared
                 Console.WriteLine();
             }
         }
-        public void GetResources()
+        internal void GetResources()
         {
+            //#1: Refactor this so that user presses a button to generate a resource. The resource is modified by the Building level.
             Console.Clear();
-            if (Storage.ResourcesList.Count == 0)
-            {
-                Console.WriteLine("You do not have any resources yet.");
-              
-                new CreateItem().Create(Storage.ResourcesList, new DTOResources().Wood);
-            }
-            else
-            {
-                new ItemInfo().ViewStatus(Storage.ResourcesList, new DTOResources().ItemType);
-                Console.WriteLine("");
-                Console.WriteLine("Please select which Resource to add to:");
-                Console.WriteLine("");
 
-                for (int i = 0; i < Storage.ResourcesList.Count; i++)
-                {
-                    Console.WriteLine($"Press {i} to add to {Storage.ResourcesList[i].ItemName}"
-
-                        );
-
-                    if (i > 9)
-                    {
-                        Console.WriteLine("Too many Resources are created. Please delete some resources.");
-                    }
+            if (Storage.ResourcesList.Any(item => item.ItemName == new DTOResources().Wood)) 
+                { 
+                    Console.WriteLine("Press 'w' to add more Wood");
                 }
-
-                int key = Convert.ToInt32(Console.ReadLine());
-
-                Console.WriteLine("---------------------------------------------------------");
-                Console.WriteLine("Please select how much resources to add");
-                float amount = Convert.ToSingle(Console.ReadLine());
-
-                //TODO: #1 add the logic to update the amount
-                Storage.ResourcesList[key].ItemCounter += amount;
-                Console.WriteLine("---------------------------------------------------------");
-                Console.WriteLine($"You now have {Storage.ResourcesList[key].ItemCounter} of {Storage.ResourcesList[key].ItemName}");
-                new UserMessages().PressAnyKeyToNavigateToMenu();
+            if (Storage.ResourcesList.Any(item => item.ItemName == new DTOResources().Stone))
+            {
+                Console.WriteLine("Press 's' to add more Stone");
             }
+            if (Storage.ResourcesList.Any(item => item.ItemName == new DTOResources().Gold))
+            {
+                Console.WriteLine("Press 'g' to add more Gold");
+            }
+
+            string key = new Extension_Methods().storeKey();
+
+            switch (key)
+            { 
+                //TODO #2: This needs to be dryer and debugged
+                case "w":
+                float addedAmount = 100;
+                if (Storage.BuildingsList[0].ItemLevel > 0) {addedAmount *= Storage.BuildingsList[0].ItemLevel;}
+                    Storage.ResourcesList[0].ItemCounter += addedAmount;
+                Console.WriteLine($"{addedAmount} added to wood. you now have {Storage.ResourcesList[0].ItemCounter} wood.");
+                break;
+            }
+
+
+
+            //if (Storage.ResourcesList.Count == 0)
+            //{
+            //    Console.WriteLine("You do not have any resources yet.");
+
+            //    new CreateItem().Create(Storage.ResourcesList, new DTOResources().Wood);
+            //}
+            //else
+            //{
+            //    new ItemInfo().ViewStatus(Storage.ResourcesList, new DTOResources().ItemType);
+            //    Console.WriteLine("");
+            //    Console.WriteLine("Please select which Resource to add to:");
+            //    Console.WriteLine("");
+
+            //    for (int i = 0; i < Storage.ResourcesList.Count; i++)
+            //    {
+            //        Console.WriteLine($"Press {i} to add to {Storage.ResourcesList[i].ItemName}"
+
+            //            );
+
+            //        if (i > 9)
+            //        {
+            //            Console.WriteLine("Too many Resources are created. Please delete some resources.");
+            //        }
+            //    }
+
+            //    int key = Convert.ToInt32(Console.ReadLine());
+
+            //    Console.WriteLine("---------------------------------------------------------");
+            //    Console.WriteLine("Please select how much resources to add");
+            //    float amount = Convert.ToSingle(Console.ReadLine());
+
+            //    Storage.ResourcesList[key].ItemCounter += amount;
+            //    Console.WriteLine("---------------------------------------------------------");
+            //    Console.WriteLine($"You now have {Storage.ResourcesList[key].ItemCounter} of {Storage.ResourcesList[key].ItemName}");
+            //    new UserMessages().PressAnyKeyToNavigateToMenu();
+            //}
         }
     }
 }
